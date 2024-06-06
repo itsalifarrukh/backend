0173378850f4d20f5ee57b3a3017e837baa51d36
farrukh135 <afarrukh302@gmail.com>
2024-06-06 16:59:05 +0500

    Logic Building | Register Controller & Bugs Fixing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { registreUser } from "../controllers/user.controller.js";
+import { uplaod } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registreUser);
+router.route("/register").post(
+   uplaod.fields([
+      {
+         name: "avatar",
+         maxCount: 1,
+      },
+      {
+         name: "coverImage",
+         maxCount: 1,
+      },
+   ]),
+   registreUser
+);
 // router.route("/login").post(loginUser);
 
 export default router;
