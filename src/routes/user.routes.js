fc6a6c90c23cb3f3ac12c503c709f18bf9111f52
farrukh135 <afarrukh302@gmail.com>
2024-06-08 16:53:52 +0500

    Fix minor bugs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -34,7 +34,7 @@ router.route("/login").post(loginUser);
 
 //secured Routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("refresh-token").post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurretUserPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
@@ -43,7 +43,7 @@ router
    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
    .route("/cover-image")
-   .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
